02a983be0e14a2efae57f14088e2e77ecbbee431
Arnold Joseph A. Fudolig <arnold.anaviso@gmail.com>
2018-07-27 23:41:21 +0800

    Updated signup page

--- react-client/src/components/UI/InputField/InputField.js
+++ react-client/src/components/UI/InputField/InputField.js
@@ -8,20 +8,48 @@ class InputField extends Component {
     super(props);
 
     this.state = {
-      showErrors: true
+      showError: false
     };
+  }
+
+  // componentWillReceiveProps(nextProps) {
+  //   if (nextProps.error) {
+  //     this.setState({ showErrors: true });
+  //   }
+  // }
+
+  static getDerivedStateFromProps(props, state) {
+    // Any time the current user changes,
+    // Reset any parts of state that are tied to that user.
+    // In this simple example, that's just the email.
+    if (props.error !== null) {
+      return {
+        showError: true
+      };
+    } else {
+      return {
+        showError: false
+      };
+    }
+    return null;
   }
 
   render() {
     let inputClasses = ['Group__input'];
 
     this.props.className ? inputClasses.push(this.props.className) : null;
-    this.props.error ? inputClasses.push('Group__input-invalid') : null;
+    this.state.showError ? inputClasses.push('Group__input-invalid') : null;
+
+    // if (this.props.touched && this.state.showError && !this.props.valid) {
+    //   inputClasses.pop();
+    // }
 
     return (
       <div className="Group">
         {this.props.error ? (
-          <p className="Group__error">{this.props.error}</p>
+          <CSSTransition classNames="fade" timeout={1000}>
+            <p className="Group__error">{this.props.error}</p>
+          </CSSTransition>
         ) : null}
         <input
           className={inputClasses.join(' ')}
@@ -30,7 +58,6 @@ class InputField extends Component {
           name={this.props.name}
           id={this.props.id}
           onChange={this.props.onChange}
-          onFocus={this.onFocus}
         />
         <label className="Group__label" htmlFor={this.props.id}>
           {this.props.label}
--- react-client/src/pages/Signup.js
+++ react-client/src/pages/Signup.js
@@ -13,11 +13,56 @@ class Signup extends Component {
     super(props);
 
     this.state = {
-      username: '',
-      first_name: '',
-      last_name: '',
-      email: '',
-      password: '',
+      form: {
+        username: {
+          value: '',
+          type: 'text',
+          placeholder: 'User Name',
+          label: 'User Name',
+          valid: false,
+          touched: false
+        },
+        first_name: {
+          value: '',
+          type: 'text',
+          placeholder: 'First Name',
+          label: 'First Name',
+          valid: false,
+          touched: false
+        },
+        last_name: {
+          value: '',
+          type: 'text',
+          placeholder: 'Last Name',
+          label: 'Last Name',
+          valid: false,
+          touched: false
+        },
+        email: {
+          value: '',
+          type: 'email',
+          placeholder: 'Email',
+          label: 'Email',
+          valid: false,
+          touched: false
+        },
+        password: {
+          value: '',
+          type: 'password',
+          placeholder: 'Password',
+          label: 'Password',
+          valid: false,
+          touched: false
+        },
+        password2: {
+          value: '',
+          type: 'password',
+          placeholder: 'Confirm Password',
+          label: 'Confirm password',
+          valid: false,
+          touched: false
+        }
+      },
       errors: {}
     };
   }
@@ -46,27 +91,52 @@ class Signup extends Component {
 
   onSubmit = e => {
     e.preventDefault();
+    const keys = Object.keys(this.state.form);
+
+    // keys.map(key => {
+    //   const updatedFormElement = {
+    //     ...key,
+    //     touched: false
+    //   };
+
+    //   const updatedForm = {
+    //     ...this.state.form,
+    //     key: updatedFormElement
+    //   };
+
+    //   this.setState({ ...this.state, updatedForm });
+    // });
 
     const newUser = {
-      username: this.state.username,
-      first_name: this.state.first_name,
-      last_name: this.state.last_name,
-      email: this.state.email,
-      password: this.state.password,
-      password2: this.state.password2
+      username: this.state.form.username.value,
+      first_name: this.state.form.first_name.value,
+      last_name: this.state.form.last_name.value,
+      email: this.state.form.email.value,
+      password: this.state.form.password.value,
+      password2: this.state.form.password2.value
     };
 
     this.props.registerUser(newUser);
   };
 
   onChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
+    const updatedFormElement = {
+      ...this.state.form[e.target.name],
+      value: e.target.value
+    };
+
+    const updatedForm = {
+      ...this.state.form,
+      [e.target.name]: updatedFormElement
+    };
+
+    this.setState({ ...this.state, form: updatedForm });
   };
 
   render() {
+    const fields = Object.keys(this.state.form);
     let errors = {};
 
-    console.log(Object.keys(this.state.errors).length);
     if (Object.keys(this.state.errors).length > 0) {
       this.state.errors.username
         ? (errors.username = this.state.errors.username[0])
@@ -96,66 +166,23 @@ class Signup extends Component {
           <h1 className="Signup__heading">Connect With Other Developers</h1>
           <form noValidate onSubmit={this.onSubmit}>
             <div className="Signup__form-container">
-              <InputField
-                className="Signup__input"
-                error={errors.username ? errors.username : null}
-                type="text"
-                id="username"
-                name="username"
-                onChange={this.onChange}
-                placeholder="User Name"
-                label="User Name"
-              />
-              <InputField
-                className="Signup__input"
-                error={errors.first_name ? errors.first_name : null}
-                type="text"
-                id="first_name"
-                name="first_name"
-                onChange={this.onChange}
-                placeholder="First Name"
-                label="First Name"
-              />
-              <InputField
-                className="Signup__input"
-                error={errors.last_name ? errors.last_name : null}
-                type="text"
-                id="last_name"
-                name="last_name"
-                onChange={this.onChange}
-                placeholder="Last Name"
-                label="Last Name"
-              />
-              <InputField
-                className="Signup__input"
-                error={errors.email ? errors.email : null}
-                type="email"
-                id="email"
-                name="email"
-                onChange={this.onChange}
-                placeholder="email@sample.com"
-                label="Email"
-              />
-              <InputField
-                className="Signup__input"
-                error={errors.password ? errors.password : null}
-                type="password"
-                id="password"
-                name="password"
-                onChange={this.onChange}
-                placeholder="Password"
-                label="Password"
-              />
-              <InputField
-                className="Signup__input"
-                error={errors.password2 ? errors.password2 : null}
-                type="password"
-                id="password2"
-                name="password2"
-                onChange={this.onChange}
-                placeholder="Confirm Password"
-                label="Confirm Password"
-              />
+              {fields.map(field => {
+                return (
+                  <InputField
+                    key={field}
+                    className="Signup__input"
+                    error={errors[field] ? errors[field] : null}
+                    type={this.state.form[field].type}
+                    id={field}
+                    name={field}
+                    placeholder={this.state.form[field].placeholder}
+                    label={this.state.form[field].label}
+                    onChange={this.onChange}
+                    touched={this.state.form[field].touched}
+                  />
+                );
+              })}
+
               <div className="Signup__submit-button">
                 <Button type="submit" />
               </div>
